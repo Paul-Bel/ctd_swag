59ea5451d4d76dd15fbed6e0a12b3b064701ad83
Paul-Bel <paul.bel.job@gmail.com>
2025-03-14 19:41:59 -0400

    week-02-components_1

--- src/TodoForm.jsx
+++ src/TodoForm.jsx
@@ -1,7 +1,7 @@
 function TodoForm() {
     return (
             <form onSubmit={(e) => e.preventDefault()}>
-                <label htmlFor={"todoTitle"}>Todo</label>
+                <label htmlFor="todoTitle">Todo</label>
                 <input id="todoTitle" type="text"/>
                 <button>"Add Todo"</button>
             </form>
